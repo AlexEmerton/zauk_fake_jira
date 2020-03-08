ee06654be6a8108c0dd9a46f9695bc9c9ef4ed3d
AlexEmerton <emerton.alex@live.com>
2020-03-08 19:29:17 +0000

    clean up imports + gitignore

--- FunPro.CW1.7586.DAL/ListOfTickets.cs
+++ FunPro.CW1.7586.DAL/ListOfTickets.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FunPro.CW1._7586.DAL
 {
